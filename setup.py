9c6239178a373121bf1c918fa3d8b50b79184357
Duelist <duelist.dev@gmail.com>
2025-04-09 16:24:00 +0300

    rename package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 setup(
-    name="arduino-hid-controller",
+    name="arduino_hid_controller",
     version='{{VERSION_PLACEHOLDER}}',
     author="Duelist",
     author_email="duelist.dev@gmail.com",
